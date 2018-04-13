a88398d7e50b68490d7ed21515594e448964db95
Anastasiia Diudina <anastasiia@holvi.com>
2018-04-13 19:18:41 +0300

    Add redirects

--- src/components/rate-user/RateUserView.js
+++ src/components/rate-user/RateUserView.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { withRouter } from "react-router-dom";
 import ScoreAction from 'components/rate-user/ScoreAction'
 
-const RateUserView = ({increaseRating, decreaseRating}) => (
+const RateUserView = ({history, increaseRating, decreaseRating}) => {
+  let redirectToSuccessView = () => {
+    history.push('rate-success')
+  }
+  return (
   <div>
     <ScoreAction
        label="Give medal"
-       onClick={increaseRating}/>
+       onClick={
+        () => {
+          increaseRating();
+          redirectToSuccessView();
+        }}/>
     <ScoreAction
-       label="Give meat"
-       onClick={decreaseRating}/>
-  </div>
-)
+      label="Give meat"
+      onClick={
+        () => {
+          decreaseRating();
+          redirectToSuccessView();
+        }
+      }
+      />
+  </div>)
+}
 
-export default RateUserView
+export default withRouter(RateUserView)
