784bffa4f88c2352a3ff6a49acf961d2fe8ca889
Vas <contact@vasily.cc>
2018-04-13 18:50:58 +0300

    Added images for score table rows

--- src/components/account/ProfileInfo.js
+++ src/components/account/ProfileInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfileInfo = () => (
   pug`
   .profile-info
-    img.profile-info-userpic(src="http://lorempixel.com/640/640/people")
+    img.profile-info-userpic(src="https://placeimg.com/200/200/people")
     .profile-info-progressbar
     .profile-info-progressbar-hint.text-center
       | 4000 medals until green belt
